86e2fd2573cb3420f01076c6950caf718f9af695
Albin Berge <albin@prologic.se>
2026-09-14 11:41:05 +0200

    Yaml workflow added test

--- src/example.py
+++ src/example.py
@@ -1,3 +1,11 @@
 # Return the square of a number
 def square(n):
     return n * n
+
+
+def add(a, b):
+    return a + b
+
+
+def multiply(a, b):
+    return a * b
--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -1,4 +1,4 @@
-from src.example import square
+from src.example import square, multiply, add
 import pytest
 
 
@@ -14,3 +14,18 @@ def test_square():
 
     # Assert
     assert actual == expected
+
+
+@pytest.mark.unit
+def test_add():
+
+    # Assert
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+
+
+@pytest.mark.unit
+def test_divide():
+
+    # Assert
+    assert multiply(10, 2) == 20
